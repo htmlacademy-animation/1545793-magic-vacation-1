936fc6fd3c1a175d599e894d698f05fcd95c0bc5
Дмитрий Шагалеев <web13spb@yandex.ru>
2021-10-21 15:14:50 +0300

    m7-t9

--- source/js/modules/three/IntroAndStory.js
+++ source/js/modules/three/IntroAndStory.js
@@ -2,18 +2,24 @@ import * as THREE from 'three';
 import SceneIntro from './introScene/SceneIntro.js';
 import SceneAllStory from './storyScene/StorySceneAll.js';
 import {loadModel} from '../three/modelLoader/modelLoader.js';
-import {animateScale, animateMoveY} from '../helpers/animations.js';
+import {animateScale, animateMoveY, animateWithFPS} from '../helpers/animations.js';
 import CameraAndLight from './CameraAndLight.js';
 import startAnim from '../startAnim.js';
+import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer';
+import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass';
+import {ShaderPass} from 'three/examples/jsm/postprocessing/ShaderPass';
+import bubbleRawShaderMaterial from './storyScene/materials/bubbleRawShaderMaterial';
 
 export const isMobile = /android|ipad|iphone|ipod/i.test(navigator.userAgent) && !window.MSStream;
 export let isLandscape;
+export let activeScene;
 
 class IntroAndStory {
   constructor() {
     this.canvasID = `screen__canvas--introAndStory`;
     this.width = window.innerWidth;
     this.height = window.innerHeight;
+    this.canvasCenter = {x: this.width, y: this.height};
     this.textureWidth = 2048;
     this.textureHeight = 1024;
     this.textureRatio = this.textureWidth / this.textureHeight;
@@ -31,6 +37,64 @@ class IntroAndStory {
     this.updateSize = this.updateSize.bind(this);
 
     this.isAnim = false;
+
+    this.roomSettings = [
+      {
+        options: {hue: 0.0}
+      },
+      {options: {
+        hue: 0.1,
+        isMagnifier: true,
+        animationSettings: {
+          hue: {
+            initalHue: 0.1,
+            finalHue: -0.5,
+            duration: 3000,
+            variation: 0.3,
+          },
+        }
+      }
+      }
+    ];
+
+    this.bubbleGlareOffset = 0.8;
+    this.bubbleGlareStartRadianAngle = 2;
+    this.bubbleGlareEndRadianAngle = 2.8;
+
+    this.bubblesDuration = 3000;
+
+    this.bubbles = [
+      {
+        radius: this.width * 0.1,
+        initialPosition: [this.canvasCenter.x - this.width * 0.5, this.canvasCenter.y - this.height * 1.8],
+        position: [this.canvasCenter.x - this.width * 0.5, this.canvasCenter.y - this.height * 1.8],
+        finalPosition: [this.canvasCenter.x - this.width * 0.5, this.canvasCenter.y + this.height * 1.8],
+        amplitude: 80,
+        glareOffset: this.bubbleGlareOffset,
+        glareAngleStart: this.bubbleGlareStartRadianAngle,
+        glareAngleEnd: this.bubbleGlareEndRadianAngle
+      },
+      {
+        radius: this.width * 0.15,
+        initialPosition: [this.canvasCenter.x - this.width * 0.18, this.canvasCenter.y - this.height * 1.3],
+        position: [this.canvasCenter.x - this.width * 0.18, this.canvasCenter.y - this.height * 1.3],
+        finalPosition: [this.canvasCenter.x - this.width * 0.18, this.canvasCenter.y + this.height * 2.6],
+        amplitude: -100,
+        glareOffset: this.bubbleGlareOffset,
+        glareAngleStart: this.bubbleGlareStartRadianAngle,
+        glareAngleEnd: this.bubbleGlareEndRadianAngle
+      },
+      {
+        radius: this.width * 0.05,
+        initialPosition: [this.canvasCenter.x, this.canvasCenter.y - this.height * 2],
+        position: [this.canvasCenter.x, this.canvasCenter.y - this.height * 2],
+        finalPosition: [this.canvasCenter.x, this.canvasCenter.y + this.height * 1.2],
+        amplitude: 60,
+        glareOffset: this.bubbleGlareOffset,
+        glareAngleStart: this.bubbleGlareStartRadianAngle,
+        glareAngleEnd: this.bubbleGlareEndRadianAngle
+      },
+    ];
   }
 
   init() {
@@ -41,9 +105,11 @@ class IntroAndStory {
     this.canvas.width = this.width;
     this.canvas.height = this.height;
 
+    this.sceneSize = new THREE.Vector2();
+
     this.isLandscape();
 
-    this.renderer = new THREE.WebGLRenderer({canvas: this.canvas});
+    this.renderer = new THREE.WebGLRenderer({canvas: this.canvas}); // logarithmicDepthBuffer: true
     this.renderer.setClearColor(0x5f458c, 1);
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.width, this.height);
@@ -56,6 +122,22 @@ class IntroAndStory {
     this.camera = new THREE.PerspectiveCamera(this.cameraFov, this.cameraAspect, 0.1, 5000);
 
     this.addScene();
+
+    this.composer = new EffectComposer(this.renderer);
+    this.composer.setPixelRatio(window.devicePixelRatio);
+    this.composer.setPixelRatio(window.devicePixelRatio);
+    const renderPass = new RenderPass(this.scene, this.camera);
+    this.composer.addPass(renderPass);
+
+    this.getEffectMaterial = (texture) => new THREE.RawShaderMaterial(bubbleRawShaderMaterial({
+      map: {value: texture},
+      options: {value: this.roomSettings[1].options},
+      ...this.addBubble(1),
+    }));
+    this.effectMaterial = this.getEffectMaterial();
+
+    const effectPass = new ShaderPass(this.effectMaterial, `map`);
+    this.composer.addPass(effectPass);
 
     this.cameraAndLight = new CameraAndLight(this.camera, this.introGroupObj, this.SceneAllStory);
 
@@ -77,6 +159,29 @@ class IntroAndStory {
       this.cameraAndLight.setCameraRotation(coef);
     });
     this.render();
+  }
+
+  getSceneSize() {
+    this.renderer.getSize(this.sceneSize);
+    return this.sceneSize;
+  }
+
+  addBubble(index) {
+    const {width, height} = this.getSceneSize();
+    const pixelRatio = this.renderer.getPixelRatio();
+
+    if (this.roomSettings[index].options.isMagnifier) {
+      return {
+        magnification: {
+          value: {
+            bubbles: this.bubbles,
+            resolution: [width * pixelRatio, height * pixelRatio],
+          }
+        },
+      };
+    }
+
+    return {};
   }
 
   addScene() {
@@ -186,6 +291,7 @@ class IntroAndStory {
 
   setStory(sceneID) {
     this.activeScene = sceneID;
+    activeScene = this.activeScene;
 
     const duration = 500;
 
@@ -210,18 +316,32 @@ class IntroAndStory {
         break;
       case `scene0`:
         angle = 0;
+        this.resetHueShift(0);
+        this.resetBubbles();
         this.cameraAndLight.setCameraStory(angle, duration, `easeLinear`);
         break;
       case `scene1`:
         angle = 90;
+        this.resetHueShift(1);
+        this.animateHueShift();
+
+        this.resetBubbles();
+        this.animateBubbles(0);
+        this.animateBubbles(1);
+        this.animateBubbles(2);
+
         this.cameraAndLight.setCameraStory(angle, duration, `easeLinear`);
         break;
       case `scene2`:
         angle = 180;
+        this.resetHueShift(0);
+        this.resetBubbles();
         this.cameraAndLight.setCameraStory(angle, duration, `easeLinear`);
         break;
       case `scene3`:
         angle = 270;
+        this.resetHueShift(0);
+        this.resetBubbles();
         this.cameraAndLight.setCameraStory(angle, duration, `easeLinear`);
         break;
     }
@@ -240,7 +360,8 @@ class IntroAndStory {
   }
 
   render() {
-    this.renderer.render(this.scene, this.camera);
+    // this.renderer.render(this.scene, this.camera);
+    this.composer.render();
 
     this.animIntroScene();
     this.SceneAllStory.animationsScene(this.activeScene);
@@ -273,6 +394,72 @@ class IntroAndStory {
     this.renderer.setSize(this.width, this.height);
 
     this.sceneIntro.setPositionIntroObj();
+
+    const {width, height} = this.getSceneSize();
+    const pixelRatio = this.renderer.getPixelRatio();
+    const resolution = [width * pixelRatio, height * pixelRatio];
+    this.effectMaterial.uniforms.magnification.value.resolution = resolution;
+  }
+
+  resetHueShift(index) {
+    this.roomSettings[index].options.hue = this.roomSettings[index].options.hue;
+  }
+
+  hueShiftIntensityAnimationTick(from, to) {
+    return (progress) => {
+      let hueShift;
+      if (progress < 0.5) {
+        hueShift = from + progress * (to - from);
+      } else {
+        hueShift = to + progress * (from - to);
+      }
+      this.roomSettings[1].options.hue = hueShift;
+    };
+  }
+
+  animateHueShift() {
+    const {initalHue, finalHue, duration, variation} = this.roomSettings[1].options.animationSettings.hue;
+    const offset = (Math.random() * variation * 2 + (1 - variation));
+    let anim = () => {
+      animateWithFPS(this.hueShiftIntensityAnimationTick(initalHue, finalHue * offset), duration * offset, 30, () => {
+        if (this.activeScene === `scene1`) {
+          anim();
+        }
+      });
+    };
+    anim();
+  }
+
+  resetBubbles() {
+    this.bubbles.forEach((_, index) => {
+      this.bubbles[index].position = [...this.bubbles[index].initialPosition];
+    });
+  }
+
+  bubblePositionAnimationTick(bubble, from, to) {
+    return (progress) => {
+      const y = from[1] + progress * (to[1] - from[1]);
+      const offset = bubble.amplitude * Math.pow(1 - progress, 1) * Math.sin(progress * Math.PI * 10);
+      const x = (offset + bubble.initialPosition[0]);
+
+      bubble.position = [x, y];
+    };
+  }
+
+  animateBubbles(index) {
+    let anim = () => {
+      animateWithFPS(
+          this.bubblePositionAnimationTick(this.bubbles[index], this.bubbles[index].initialPosition, this.bubbles[index].finalPosition),
+          this.bubblesDuration,
+          30,
+          () => {
+            if (this.activeScene === `scene1`) {
+              anim();
+            }
+          }
+      );
+    };
+    anim();
   }
 }
 
